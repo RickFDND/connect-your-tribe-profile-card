0bf2cf81f2f1a2ecdd9b6fca91ee4575d4fe138f
RickFDND <rick.pijnacker@hva.nl>
2025-02-03 12:00:13 +0100

    j

--- server.js
+++ server.js
@@ -67,3 +67,5 @@ app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+
